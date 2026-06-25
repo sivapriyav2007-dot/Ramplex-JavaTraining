60337ccc525ac65dd7d24be03684af9a52e24387
sivapriya <sivapriyav2007@gmail.com>
2026-06-25 10:22:12 +0530

    Program created for swap numbers

---
+++ src/day_1/swap.java
@@ -0,0 +1,15 @@
+package day_1;
+
+class swap{
+    public static void main(String [] args){
+        int a=5;
+        int b=10;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+
+        System.out.println(b);
+    }
+}
+
