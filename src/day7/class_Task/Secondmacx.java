97f315600e16bf30803582815579e3dd7c3ec052
sivapriya <sivapriyav2007@gmail.com>
2026-06-27 15:43:44 +0530

    java program to print second maximum number

---
+++ src/day7/class_Task/Secondmacx.java
@@ -0,0 +1,18 @@
+package day7.class_Task;
+
+class Secondmacx {
+    public static void main(String[] args){
+        int[] arr={4,3,1,5,2};
+        int Smax=0;
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                Smax=max;
+                max=arr[i];
+            }
+        }
+        System.out.println("Second maximum number is "+Smax);
+
+    }
+
+}
