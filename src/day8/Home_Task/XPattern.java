16407a8cbc2f5207d5779c350cfcbec3f6c211be
sivapriya <sivapriyav2007@gmail.com>
2026-06-30 09:20:43 +0530

    java program to print XPattern

---
+++ src/day8/Home_Task/XPattern.java
@@ -0,0 +1,24 @@
+package day8.Home_Task;
+
+import java.util.Scanner;
+
+public class XPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || j == n - i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
