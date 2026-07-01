bdfffa12f59f8f01cd10fa1938f8935a8da896ea
sivapriya <sivapriyav2007@gmail.com>
2026-07-01 14:47:37 +0530

    java program to print Duplicates

--- src/day10/class_Task/Duplicate.java
+++ src/day10/class_Task/Duplicate.java
@@ -6,7 +6,7 @@ public class Duplicate {
         String [] arr={"sam","jane","sam","tom"};
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length;j++){
-                if(arr[i]==arr[j] && i!=j && i<j){
+                if(arr[i].equals(arr[j]) && i!=j && i<j){
                     System.out.println(arr[j]);
                 }
             }
