bebe26849a7d5c5e8a483d6409ac7eba9fd01afd
sivapriya <sivapriyav2007@gmail.com>
2026-07-02 08:13:40 +0530

    java program to print Amstrong

---
+++ src/day10/Home_Task/Amstrong.java
@@ -0,0 +1,23 @@
+package day10.Home_Task;
+
+import java.util.Scanner;
+public class Amstrong {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = sc.nextInt();
+        int original =  num ;
+        int sum  = 0;
+        while(num>0){
+            int digit = num % 10;
+            sum = sum + (digit * digit * digit);
+            num = num /10;
+        }
+        if(sum == original){
+            System.out.println("Armstrong number");
+        }else{
+            System.out.println("Not Armstrong number");
+        }
+
+    }
+}
