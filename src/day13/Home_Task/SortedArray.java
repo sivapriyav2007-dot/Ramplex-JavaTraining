0ae2c9bca68786213b103a628a539135c161c22c
sivapriya <sivapriyav2007@gmail.com>
2026-07-06 09:27:16 +0530

    java program to print SortedArray

---
+++ src/day13/Home_Task/SortedArray.java
@@ -0,0 +1,19 @@
+package day13.Home_Task;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        boolean sorted = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                sorted = false;
+                break;
+            }
+        }
+        if (sorted) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+    }
+}
