54b25dd187346b079d5db01b28da1df54e8f0551
sivapriya <sivapriyav2007@gmail.com>
2026-07-20 09:26:45 +0530

    Java program for day 4

---
+++ src/day4/Add.java
@@ -0,0 +1,9 @@
+package day4;
+
+public class Add {
+    public static void main(String[] args){
+        for(int i = 1; i <= 5; i++) {
+            System.out.print(i++ + " ");
+        }
+    }
+}
---
+++ src/day4/MultiplicationTable.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int num =7;
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
